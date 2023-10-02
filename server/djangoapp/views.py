c86ed91bb653f7f201ef0c0c8e32fc9f49ba8767
Michael Oporto <mjoporto@gmail.com>
2023-10-02 15:07:58 -0400

    updates to templates and urls

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,14 +16,20 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+def get_dealerships(request):
+  #  context = {}
+  #  if request.method == "GET":
+        #return render(request, 'djangoapp/index.html', context)
+        return render(request, 'djangoapp/index.html')
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    return render(request,'djangoapp/about.html')
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    return render(request,'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -38,10 +44,10 @@ logger = logging.getLogger(__name__)
 # ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+#def get_dealerships(request):
+  #  context = {}
+   # if request.method == "GET":
+    #    return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
